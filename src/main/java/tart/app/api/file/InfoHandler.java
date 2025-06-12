aac60f3b8f39943faae25b9846d8271ec859170c
Kirill Vinogradov <vikian050194@gmail.com>
2025-06-12 22:56:05 +0200

    initial cache version

--- src/main/java/tart/app/api/file/InfoHandler.java
+++ src/main/java/tart/app/api/file/InfoHandler.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URI;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import tart.app.api.*;
 import tart.app.errors.*;
@@ -15,6 +16,7 @@ import tart.domain.file.FileService;
 public class InfoHandler extends Handler {
 
     private final FileService fileService;
+    private final HashMap<String, InfoResponse> cache;
 
     public InfoHandler(
             FileService fileService,
@@ -23,6 +25,7 @@ public class InfoHandler extends Handler {
     ) {
         super(objectMapper, exceptionHandler);
         this.fileService = fileService;
+        cache = new HashMap<>();
     }
 
     @Override
@@ -58,6 +61,13 @@ public class InfoHandler extends Handler {
         exchange.close();
     }
 
+    private String getString(String path, DateFilter f) {
+        var years = String.join("", f.years.stream().map(y -> y.toString()).toList());
+        var months = String.join("", f.months.stream().map(m -> m.toString()).toList());
+        var days = String.join("", f.days.stream().map(d -> d.toString()).toList());
+        return path + years + months + days;
+    }
+
     private ResponseEntity<InfoResponse> doGet(URI uri) throws IOException {
         var params = splitQuery(uri.getRawQuery());
         // TODO extract magic string
@@ -74,15 +84,27 @@ public class InfoHandler extends Handler {
         var delimiter = "/";
         var path = List.of(dirPath.split(delimiter)).stream().filter(p -> !p.isEmpty()).toList();
 
-        var dirs = fileService.getDirectories(path);
-        var files = fileService.getFiles(path, filter);
-        var years = fileService.getYears(path, filter);
-        var months = fileService.getMonths(path, filter);
-        var days = fileService.getDays(path, filter);
-        var r = new InfoResponse(dirs, files);
-        r.years.addAll(years);
-        r.months.addAll(months);
-        r.days.addAll(days);
+        var s = getString(dirPath, filter);
+
+        InfoResponse r;
+
+        if (cache.containsKey(s)) {
+            r = cache.get(s);
+        } else {
+            var dirs = fileService.getDirectories(path);
+            var files = fileService.getFiles(path, filter);
+            var years = fileService.getYears(path, filter);
+            var months = fileService.getMonths(path, filter);
+            var days = fileService.getDays(path, filter);
+
+            r = new InfoResponse(dirs, files);
+            r.years.addAll(years);
+            r.months.addAll(months);
+            r.days.addAll(days);
+
+            cache.put(s, r);
+        }
+
         return new ResponseEntity<>(r,
                 getHeaders(Constants.CONTENT_TYPE, Constants.APPLICATION_JSON), StatusCode.OK);
 
